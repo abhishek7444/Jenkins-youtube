f5bbfbc24f050f367ab66533601330cb64832b52
Abhishek Jain <abhishekjain7444@gmail.com>
2025-05-07 09:06:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "my-ec2" {
   ami                    = var.ami
   instance_type          = var.instance-type
   associate_public_ip_address = true
+  subnet_id = "subnet-0c83772bdcdf35f0d"
 
   tags = {
     Name = "my-test-instance"
